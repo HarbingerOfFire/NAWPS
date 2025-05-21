253928058b2ec3cfcc63c7de27314c8fc653758b
HarbingerOfFire <HarbingerOfFire@proton.me>
2025-05-20 22:29:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main_cli(conf=None):
     import cli
     with open(conf, 'r') as file:
         config_str = file.read()
-        cli.apply_registry_config(config_str)
+        cli.write_registry_from_config(config_str)
 
 def main(conf=None):
     if conf is None:
@@ -16,6 +16,24 @@ def main(conf=None):
 
 
 if __name__ == "__main__":
+
+    import ctypes
+    import sys
+    
+    def is_admin():
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+    
+    if not is_admin():
+        print("Not running as admin. Trying to elevate...")
+        ctypes.windll.shell32.ShellExecuteW(
+            None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        sys.exit()
+    
+    print("Running with admin privileges.")
+
     try:
         conf = sys.argv[1]
         main(conf)
